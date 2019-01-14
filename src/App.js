4f16fa43a1c73b1fa5a62b33ed6f400bda1d444a
John Caldwell <jmcald0@gmail.com>
2019-01-13 22:44:07 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+import Card from "./components/card";
+import picture from "./picture.json"
 import './App.css';
 
 class App extends Component {
+  state = {
+    picture,
+    clickedPicture: [],
+    tally: 0
+  };
+
+  handleClick = event => {
+    const currentPic = event.target.alt;
+    const clickedPic = this.state.clickedPicture.indexOf(currentPic) > -1;
+
+    if (clickedPic) {
+      this.setState({
+        picture: this.state.picture.sort(function(a, b) {
+          return 0.5 - Math.random();
+        }),
+        clickedPicture: [],
+        tally: 0
+      });
+      alert("Wroooooong! Play again?");
+    } else {
+      this.setState(
+        { picture: this.state.picture.sort(function(a, b) { return 0.5 - Math.random();
+        }),
+      clickedPicture: this.state.clickedPicture.concat(
+        currentPic
+      ),
+      tally: this.state.tally + 1
+    },
+    () => {
+      if (this.state.tally === 12) {
+        alert("You wooooooon!");
+        this.setState({ picture: this.state.picture.sort(function(a, b) {
+          return 0.5 - Math.random();
+        }),
+        clickedPicture: [],
+        tally: 0  
+        });
+      }
+    }
+      );
+    }
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Navbar
+          tally={this.state.tally}
+        />
+        <h1 className="offset-5s">
+          Click the Pics!
+        </h1>
+        <div className="wrapper">
+          {this.state.picture.map(picture => (
+            <Card
+              handleClick={this.handleClick}
+              id={picture.id}
+              key={picture.id}
+              image={picture.image}
+            />
+          ))}
+        </div>
+        <Footer />    
       </div>
     );
   }
---
+++ src/components/footer/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => (
+	<header className = "footer">
+		<p>
+			
+		</p>
+	</header>
+);
+
+export default Footer;
